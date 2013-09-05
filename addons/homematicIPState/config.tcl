b8d80ba9715f5362fa2d55d7046b6769efc27603
Norman Leutner <n.leutner@all2e.com>
2013-09-05 06:50:29 +0200

    added hostname and IP example

--- addons/homematicIPState/config.tcl
+++ addons/homematicIPState/config.tcl
@@ -6,6 +6,11 @@
 #
 
 array set IPStatusHosts {
-    1 iphonenorman
-    2 iphonesimone
+    1 192.168.2.21
+    2 192.168.2.22
 }
+
+#array set IPStatusHosts {
+#    1 iphonenorman
+#    2 iphonesimone
+#}
