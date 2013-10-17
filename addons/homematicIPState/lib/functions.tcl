360ed24fd657567177373e218f58c1bcb7751457
Blurline <j.guenzel@all2e.com>
2013-10-17 13:44:47 +0200

    ping -w 1 hinzugefügt anstatt default 10 Skeunden zu warten

--- addons/homematicIPState/lib/functions.tcl
+++ addons/homematicIPState/lib/functions.tcl
@@ -3,7 +3,7 @@
 # Check if host is is reachable
 proc pingCheck {host} {
 
-    if {![catch {exec ping -c 1 -s 1 $host}] } then {
+    if {![catch {exec ping -c 1 -s 1 -w 1 $host}] } then {
         return 1
     }
     # offline
